dd89c64687821fe29a498fb7e30a207c29b70ae2
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-06-29 11:23:56 -0700

    Moved some prefabs, added clouds & vanish effects
    Moved some of the prefabs into seperate folders, added vanish and poof effects to targets.

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private Target[] _targets;
+
+    private void OnEnable()
+    {
+        _targets = FindObjectsOfType<Target>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.Physics2DModule;
+
+public class Target : MonoBehaviour
+{
+	//Variables
+	// Public
+	//Private
+	[SerializeField] private GameObject _cloudParticlePrefab;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision started!");
+		// TODO: Change the name of the object, and extend if portion to outside
+		FlappyFatBirdBehavior2 projectile = collision.collider.GetComponent<FlappyFatBirdBehavior2>();
+		// Checks if the object is of defined type (in sharp brackets <>)
+		if (projectile !=null)
+		{
+			// Destroy the current object
+			Debug.Log("The projectile has destroyed the target!");
+            MakePuffOfClouds();
+			Destroy(gameObject);
+			return;
+		}
+
+		// Check if the target was just hit by another target
+		Target target = collision.collider.GetComponent<Target>();
+		if(target != null)
+			return; // we stop checking
+		
+		// We check if the target was collided from above just enough
+		if( collision.contacts[0].normal.y < -0.5 )
+		{
+			Debug.Log("Something that isn't a projectile or a target has destroyed the target from above!");
+			MakePuffOfClouds();
+			Destroy(gameObject);
+		}
+		
+		return;
+		
+	}
+
+	private void MakePuffOfClouds()
+    {
+		Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+		return;
+    }
+
+	private void DestroyWithPuff()
+	{
+        MakePuffOfClouds();
+		Destroy(gameObject);
+	}
+}
