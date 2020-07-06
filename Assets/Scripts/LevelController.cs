1c82d15c0ed9aaa72c463c97b4648d2898a1b894
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-07-06 06:53:12 -0700

    Edited LevelController attempt to show score

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,7 +9,7 @@ public class LevelController : MonoBehaviour
 {
     // public variables
     // TODO: Variables set outside of this class, and altered outside
-    public UnityEngine.UI.Button winButton;
+    public Text winText;
     // Counters for the
     public int projectilesLeft;
     public int targetsLeft;
@@ -18,13 +19,14 @@ public class LevelController : MonoBehaviour
     // TODO: Make index start from whichever level we're on
     private static int _nextLevelIndex = 1;
     private Target[] _targets;
-    private float totalTime = 0.0F;
+    private float _totalTime = 0.0F;
+    private bool _levelComplete = false;
 
     private void OnEnable()
     {
         _targets = FindObjectsOfType<Target>();
         targetsLeft = _targets.Count();
-
+        winText.text = "";
     }
     // Start is called before the first frame update
     /* void Start()
@@ -35,7 +37,14 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // TODO: put checks in big if block, so that update only has to check the boolean value when level is done)
+        // increment timer
+        if (!_levelComplete)
+        {
+            _totalTime = Time.deltaTime;
+        }
         // Check if there is at least one target in the list of target objects
+        
         foreach (Target target in _targets)
         {
             if (target != null)
@@ -43,9 +52,22 @@ public class LevelController : MonoBehaviour
         }
         // This part only reached when there are no targets found
         Debug.Log("All the targets are gone!");
+        // We enable going to the next level
+        if (!_levelComplete)
+        {
+            _levelComplete = true;
+            winText.text = String.Format("You win!\n" +
+                "Targets hit: {0}\n" +
+                "Shots bonus: \n" +
+                "Time bonus: " +
+                "Total: ",
+                _targetsLeft);
+        }
         // We go to the next level
+        /*
         _nextLevelIndex++;
         string nextLevelName = "Level" + _nextLevelIndex;
         SceneManager.LoadScene(nextLevelName);
+        */
     }
 }
