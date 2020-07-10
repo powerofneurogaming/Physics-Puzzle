b0e28ddb10972e6302afd09cc4d297d566085e79
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-07-10 08:21:45 -0700

    Moved backgrounds to spirtes folder, minor edits

--- Assets/Scripts/Helper Scripts/SceneLoadingHelper.cs
+++ Assets/Scripts/Helper Scripts/SceneLoadingHelper.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
+// TODO: See if MonoBehavior is required
 public class SceneLoadingHelper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Loads scene and returns true if it exists, otherwise returns false
+    public bool TryLoadingScene(string sceneName)
     {
-        
+        Debug.Log("TryLoadingScene (from SceneLoadingHelper) has started!");
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            Debug.LogFormat("Found scene {0}! Now loading!", sceneName);
+            SceneManager.LoadScene(sceneName);
+            return true;
+        }
+        else
+        {
+            Debug.LogFormat("Did not find scene {0}! Either it doesn't exist, or it isn't in the build.", sceneName);
+        }
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
