c892d535e7a160bcd082ee07d86b4d29ff64c604
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-07-21 08:17:25 -0700

    Edited canvases to adjust on differnt resolutions (the buttons), projectile & customer count

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,9 +9,14 @@ using UnityEngine.SceneManagement;
  */
 public class SceneController : MonoBehaviour
 {
-    private SceneParsing _sp;
+    private SceneParsing _sp = new SceneParsing();
+    private SceneLoading _sl = new SceneLoading();
     private static int World = 0; // {get; set; }  //= 0; // 
-    private static int Level = 0; // { get; set; }
+    private static int Level = 0; // { get; set; 
+    private static string _currentScene = "Main Menu";  // assuming start from Main Menu
+    private static string _previousScene = "Main Menu";
+    // Since the Options menu can be from anywhere, we have to memorize the scene that was brought from there
+    private static string _returnFromOptionsScene = "Main Menu";
 
     // TODO: replace getters and setters with shorthand
     public int getWorld(){return World;}
@@ -19,24 +24,39 @@ public class SceneController : MonoBehaviour
     public int getLevel() { return Level; }
     public void setLevel(int newLevel) => Level = newLevel;
 
+    //Almost the same as SceneLoading's LoadNextLevel, but it takes in the inputs
+    public void GoToNextLevel()
+    {
+        _sl.LoadNextLevel(worldNo: World, levelNo: Level);
+    }
+
+    public void LoadPreviousScene()
+    {
+        _sl.TryLoadingScene(sceneName: _previousScene);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         // Seet the world values upon starting - i.e. every time a scene is loaded
-        _sp = new SceneParsing();
-        string currentScene = SceneManager.GetActiveScene().name;
-        Debug.LogFormat("Getting info on scene {0}", currentScene);
-        // check based on the most common levels
-        if(_sp.FindLevelWord(currentScene))
+        // _sp = new SceneParsing();
+
+        // next is used temporarily, in relation to the previous scenes we've loaded
+        string nextScene = SceneManager.GetActiveScene().name;
+        Debug.LogFormat("Getting info on scene {0}", nextScene);
+
+        if(!nextScene.Equals(_currentScene)) // if we are loading a different scene
         {
-            World = _sp.FindLevelFromScene(currentScene);  // runtime error starts here (in Level 1-1)
-            Level=_sp.FindWorldFromScene(currentScene);
+            // We've found a new scene, so switch the 
+            _previousScene = _currentScene;
+            _currentScene = nextScene;
         }
-        else if(_sp.FindWorldWord(currentScene))
+        // check based on the most common levels
+        if (_sp.FindIfHasWorldLevel(_currentScene)) // _sp.FindLevelWord(_currentScene))
         {
-            World = _sp.FindLevelFromScene(currentScene);
-            Level = 1;
+            (World, Level) = _sp.FindWorldLevelFromScene(_currentScene);
+            // World = _sp.FindLevelFromScene(_currentScene);
+            // Level=_sp.FindWorldFromScene(_currentScene);
         }
         else
         {
