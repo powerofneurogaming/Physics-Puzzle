37b389a6f5209ff39ef4929408ff419893d5e330
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-07-22 08:15:17 -0700

    Minor edits, made "chef" reappear

--- Assets/Scripts/Helper Scripts/SceneLoading.cs
+++ Assets/Scripts/Helper Scripts/SceneLoading.cs
@@ -132,6 +132,20 @@ public class SceneLoading // : MonoBehaviour
         return LoadNextSceneType(sceneType: "Level", worldNo, levelNo);
     }
 
+    public int LoadNextLevel2(int worldNo, int levelNo)
+    {
+        // int startingWorld = worldNo;
+        int levelLoadedLevel = LoadNextSceneType(sceneType: "Level", worldNo, levelNo);
+        if (levelLoadedLevel <= 0) // if we didn't load the level
+        {
+            int sceneLoadedLevel = LoadNextWorld(worldNo, levelNo);
+            return sceneLoadedLevel * 2;
+        }
+        else
+            return levelLoadedLevel;
+    }
+
+    // TODO: Make function go to the next worldNo, not levelNo
     public int LoadNextWorld(int worldNo, int levelNo)
     {
         int startingWorld = worldNo;
