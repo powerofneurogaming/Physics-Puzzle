07acfea9eed551a2ad3e728db69eb2a7faaf4322
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-07-27 06:37:31 -0700

    New animated seagull

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private bool platePressed = false;
+    public bool PlatePressed
+    {
+        get { return platePressed; }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        platePressed = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        platePressed = false;
+    }
+
+}
---
+++ Assets/Scripts/Switch Behaviors/Pit.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Behavior for a pit object.
+ * "Resets" projectiles, disables everything else.
+ *  Level ends when a customer or the player hits the pit.
+ */
+public class Pit : MonoBehaviour
+{
+    public LevelController levelController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Pit collision started!");
+        Debug.Log("Checking if it is a projectile");
+        Projectile projectile = collision.collider.GetComponent<Projectile>();
+        if (projectile != null)
+        {
+            projectile.ResetProjectile();
+            return;
+        }
+        else
+        {
+            // 
+            Player player = collision.collider.GetComponent<Player>();
+            if (player != null)
+            {
+                levelController.SetResultsButton(isWin: false, "Player fell into pit!");
+                player.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                return; // Cannot destroy/disable player, as that will eliminate the camera!
+            }
+            else
+            {
+                Target target = collision.collider.GetComponent<Target>();
+                if (target != null)
+                {
+                    levelController.SetResultsButton(isWin: false, "Customer safety hazard!");
+                }
+            }
+        }
+
+        collision.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
