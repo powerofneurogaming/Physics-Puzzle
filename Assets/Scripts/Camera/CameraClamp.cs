ff17f4af03e2a6026048af6e5a3b476c2f931bda
Christopher Elliot Naschke <66655188+PoNGlyGent@users.noreply.github.com>
2020-07-30 09:27:40 -0700

    Activated/placed non-child Main Camera in scenes.

---
+++ Assets/Scripts/Camera/CameraClamp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attatck to camera.
+public class CameraClamp : MonoBehaviour
+{
+    [SerializeField] private Transform targetToFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(targetToFollow.position.x, -5.5f, 5.5f),
+            Mathf.Clamp(targetToFollow.position.y, -5f, 5f),
+            transform.position.z);
+    }
+}
